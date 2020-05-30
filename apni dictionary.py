6093ec61e18c534c8555fefe70c71dc2d96d79b1
salman449 <66164188+salman449@users.noreply.github.com>
2020-05-30 16:11:12 +0500

    add new project

---
+++ apni dictionary.py
@@ -0,0 +1,12 @@
+d = {"bothering":"take the trouble to do something.",
+     "set":"put,lay or stand(something) in a specified place or position",
+     "occasionally":"at infrequent or irregular intervals; now and then.",
+     "lure":"tempt(aperson or an animal) to do something or to go someehere,"
+    "especially by offering from of reward.","mourn":"feel or show deep"" sorrow or regret "
+    " for (someone or their death), typically by following conventions such as the wearing "
+     " of black clothes.","anticipated":"regard as probable; expect or predict.",
+     "feasible":"possible to do easily or conveniently.",
+     "predict":"say or estimate that (a specified thing) will happen in the future "
+     "or will be a consequence of something."}
+print("enter the word")
+print(d[input()])
